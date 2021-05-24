bd268846fb901c87533cbb2c23d2091ff208c610
RaulLuncariu <raul.luncariu00@e-uvt.ro>
2021-05-24 15:51:34 +0300

    Combine class update
    Updated consume function so the player part of combine can level up and win

--- Event Driven Application/src/com/company/Combine.java
+++ Event Driven Application/src/com/company/Combine.java
@@ -10,19 +10,21 @@ public class Combine {
     public Integer iron = 0;
     public Integer wood = 0;
     public Integer stone = 0;
+    public int quantity = 20;
+    public int level = 1;
 
 
     public void produce() throws InterruptedException {
         Random rand = new Random();
             while (true) {
                 synchronized (this) {
-                    int ir = rand.nextInt(20);
+                    int ir = rand.nextInt(quantity);
                     iron_l.add(ir);
                     System.out.println("Added " + ir + " iron");
-                    int st = rand.nextInt(20);
+                    int st = rand.nextInt(quantity);
                     stone_l.add(st);
                     System.out.println("Added " + st + " stone");
-                    int wd = rand.nextInt(20);
+                    int wd = rand.nextInt(quantity);
                     wood_l.add(wd);
                     System.out.println("Added " + wd + " wood");
 
@@ -40,14 +42,44 @@ public class Combine {
                 while (iron_l.size() == 0 || wood_l.size() == 0 || stone_l.size() == 0) {
                     wait();
                 }
-                iron += iron_l.getFirst();
-                wood += wood_l.getFirst();
-                stone += stone_l.getFirst();
+                iron += iron_l.removeFirst();
+                wood += wood_l.removeFirst();
+                stone += stone_l.removeFirst();
                 System.out.println("Consumed " + iron + " iron " + stone + " stone and " + wood + " wood");
+
+                if (iron >= 100 && wood >= 100 && stone >= 100){
+                    iron -= 100;
+                    wood -= 100;
+                    stone -= 100;
+                    quantity += 20;
+                    level += 1;
+                    System.out.println("Leveled up to level "+level);
+                }
+                if (level >= 5){
+                    System.out.println("YOU WON");
+                    System.exit(0);
+                }
 
                 notify();
 
                 Thread.sleep(1000);
             }
     }
+   /* public void level_up() throws InterruptedException{
+        while(true) {
+            synchronized (this) {
+                if (iron == 100 && wood == 100 && stone == 100){
+                    System.out.println("Level up");
+                    iron -= 100;
+                    wood -= 100;
+                    stone -= 100;
+                    quantity += 20;
+                    level += 1;
+                }
+                if (level >= 5){
+                    System.out.println("YOU WON");
+                }
+            }
+        }
+    }*/
 }
